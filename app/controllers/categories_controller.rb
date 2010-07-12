43d56d40b2353bc2b5ae0a85914eb5ce3a358c9e
Brian Stack <bis12@case.edu>
2010-07-11 23:39:18 -0400

    Add a category manager.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
 	def index
-		@ideas = Category.find(:all, :order => "name")
+		#@ideas = Category.find(:all, :order => "name")
+		@tags = Category.all
 
 		
     		respond_to do |format|
@@ -17,6 +18,26 @@ class CategoriesController < ApplicationController
    		   format.html # show.html.erb 
   		   format.xml  { render :xml => @ideas }
     		end
+	end
+
+	def new
+		@tag = Category.new
+                respond_to do |format|
+      		  format.html # new.html.erb
+      		  format.xml  { render :xml => @tag }
+    		end
+	end
+
+	def create
+		@tag = Category.create
+
+		respond_to do |format|
+      		  if @tag.save
+    		    format.html { redirect_to(categories_path) }
+                  else
+                    format.html { render :action => "new" }
+                  end
+                end
 	end
 
 end
